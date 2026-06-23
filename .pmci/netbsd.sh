60f405d47a556c724cf14061aa865574810c7485
John Buluba <buluba89@gmail.com>
2026-06-23 03:01:17 +0300

    ci: remove dead AppVeyor, .pmci, and Dockerfile artifacts
    .appveyor.yml is pre-modules upstream Windows CI (GOPATH clone_folder,
    GODEBUG=cgocheck=2, mingw 6.3.0, a VS2015/WDK hack) with no AppVeyor
    project connected; GitHub Actions already covers Windows (cgo + nocgo).
    .pmci/{freebsd,netbsd}.sh are upstream's manual GOPATH-era BSD scripts,
    wired into no CI (netbsd's test is even commented out). Dockerfile is an
    xgo cross-build pulling osxfuse 3.8.3 / winfsp 1.2, referenced nowhere.

--- .pmci/netbsd.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-
-# cgofuse: build and test
-export GOPATH=/tmp/go
-mkdir -p /tmp/go/src/github.com/winfsp
-cp -R /tmp/repo/cgofuse /tmp/go/src/github.com/winfsp
-cd /tmp/go/src/github.com/winfsp/cgofuse
-go build -v ./...
-# go test -v ./fuse -run 'TestUnmount|TestSignal'
